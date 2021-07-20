7703c6ac8d83ee93c433c34722f3c4a1e61c77f6
Alfredo Peña <aalfredoplz@gmail.com>
2021-07-19 20:55:20 -0400

    format and fragment

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import { Fragment } from 'react';
+
 const Accordion = ({items}) => {
     const renderedItems = items.map(item=>{
-        return <div key={item.title}>
-            <div className="title active">
-                <i className="dropdown icon"></i>
-                {item.title}
-            </div>
-            <div className="content active">
-                <p>{item.content}</p>
-            </div>
-        </div>
+        return (
+            <Fragment key={item.title}>
+                <div className="title active">
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    <p>{item.content}</p>
+                </div>
+            </Fragment>
+        )    
     })
 
-    return ( <div className="ui styled accordion">
-        {renderedItems}
-        </div> );
+    return ( 
+        <div className="ui styled accordion">
+            {renderedItems}
+        </div>
+    );
         
 }
  
