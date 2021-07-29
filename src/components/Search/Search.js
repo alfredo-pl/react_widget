868710ca047ed9cddb706a2aebd36eb112e8cb7c
Alfredo Peña <aalfredoplz@gmail.com>
2021-07-29 19:14:36 -0400

    api request setting wikipedia through function searchwiki

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,27 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 const Search = () => {
     const [term, setTerm] = useState('');
-    console.log("una vez")
+  
     useEffect(()=>{
-        console.log("un efecto")
-    },[term])
+        
+        const searchwiki = async () =>{
+             await axios.get("https://en.wikipedia.org/w/api.php",{
+                params:{
+                    action: 'query',
+                    list:'search',
+                    origin:'*',
+                    format: 'json',
+                    srsearch: term
+                },
+                
+             });
+        }
+
+        searchwiki();
+    },[term]);
+    
     return (
         <div>
             <div className="ui form">
