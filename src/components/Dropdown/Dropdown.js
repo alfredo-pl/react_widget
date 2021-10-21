51cbbf223dfaefcbe2ff2059186fa0db5e7c4f89
Alfredo Peña <aalfredoplz@gmail.com>
2021-10-21 19:45:32 -0300

    useEffect and useRef to catch event in DOM and close dropdown via it

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,8 +1,26 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
 
     const [open, setOpen] = useState(false);
+    const ref = useRef();
+    useEffect(() => {
+        const onBodyClick = (event) => {
+          if (ref.current.contains(event.target)) {
+            return;
+          }
+          setOpen(false);
+        };
+        document.body.addEventListener("click", onBodyClick, { capture: true });
+     
+        return () => {
+          document.body.removeEventListener("click", onBodyClick, {
+            capture: true,
+          });
+        };
+      }, []);
+
+
     const renderedOptions = options.map((option)=>{
 
         if(option.value === selected.value){
@@ -21,7 +39,7 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
     return ( 
         <div>
             <h1>Dropdown</h1>
-            <div className="ui form">
+            <div ref={ref} className="ui form">
                 <div className="field">
                     <label className="label">Select a Color</label>
                     <div 
@@ -35,6 +53,7 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
                     </div>
                 </div>
             </div>
+            <h3 className={`ui ${selected.value} header`}>Text here!</h3>
         </div>
      );
 }
