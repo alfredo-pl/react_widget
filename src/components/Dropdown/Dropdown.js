84df2d1b89fb73bdb2ac4744c67aa38ebb0ae636
Alfredo Peña <aalfredoplz@gmail.com>
2021-10-20 21:18:50 -0300

    props options and ui form to dropdown

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
-const Dropdown = () => {
+const Dropdown = ({options, selected, onSelectedChange}) => {
+    const renderedOptions = options.map((option)=>{
+        return(
+            <div key={option.value} 
+            onClick={()=>onSelectedChange(option)} 
+            className="item">
+            {option.label}        
+        </div>
+        )
+    } )
+    
     return ( 
         <div>
             <h1>Dropdown</h1>
+            <div className="ui form">
+                <div className="field">
+                    <label className="label">Select a Color</label>
+                    <div className="ui selection dropdown visible active">
+                        <i className="dropdown icon"></i>
+                        <div className="text">{selected.label}</div>
+                        <div className="menu visible transition">
+                            {renderedOptions}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
