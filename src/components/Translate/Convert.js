e68ea85274adb8345763bc1b8b14c65d0b593537
Alfredo Peña <aalfredoplz@gmail.com>
2021-10-23 11:48:48 -0300

    new component Convert

---
+++ src/components/Translate/Convert.js
@@ -0,0 +1,11 @@
+import React,{useEffect, useState} from 'react';
+
+const Convert = () => {
+    return ( 
+        <div>
+            Convert
+        </div>
+     );
+}
+ 
+export default Convert;
