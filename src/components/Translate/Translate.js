45dec4dd7eb4f1e5534a90efa5b5deba0b6e6566
Alfredo Peña <aalfredoplz@gmail.com>
2021-10-23 11:35:49 -0300

    style ui form and state text

--- src/components/Translate/Translate.js
+++ src/components/Translate/Translate.js
@@ -15,12 +15,21 @@ const options = [
         value: 'hi'
     }
 ];
-
+/* AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM */
 const Translate = () => {
     const [languaje,setLanguaje] = useState(options[0])
-    
+    const [text, setText] = useState('');
     return ( 
         <div>
+            <div className="ui form">    
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input 
+                        value={text}
+                        onChange={(e)=>setText(e.target.value)}
+                    />
+                </div>
+            </div>
             <Dropdown 
             label="Select at languaje"
             options={options}
